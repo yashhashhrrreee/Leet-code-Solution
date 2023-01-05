34832dc97a7a645185eed25deda25e1870049f2c
Yashashree Bedmutha <101212250+yashhashhrrreee@users.noreply.github.com>
2023-01-06 00:52:32 +0530

    Time: 68 ms (5.24%), Space: 13.9 MB (6.79%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.py
@@ -0,0 +1,13 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if len(word)==1: return True
+        if word[0].isupper() and word[1].isupper():
+            for i in range(2,len(word)):
+                if word[i].islower():
+                    return False
+            return True
+        else:
+            for i in range(1,len(word)):
+                if word[i].isupper():
+                    return False
+            return True
