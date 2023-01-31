52d0146a5229f85941d70c7501f2e276f85ef6e1
Yashashree  Bedmutha <yashashree.bedmutha@gmail.com>
2023-01-31 19:34:58 +0530

    Time: 4 ms (83.02%), Space: 40.2 MB (98.01%) - LeetHub

---
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean halvesAreAlike(String s) {
+    
+        var set=Set.of('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
+        int a=0;
+        int b=0;
+        
+        for(int i=0,j=s.length()-1;i<j;i++,j--){
+            a+=set.contains(s.charAt(i))?1:0;
+            b+=set.contains(s.charAt(j))?1:0;
+        }
+        
+        return a==b;
+    }
+}
