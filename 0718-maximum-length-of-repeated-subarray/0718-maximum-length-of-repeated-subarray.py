0aa71d422c4ae54de1b8db26d1e87d0fdde9af2b
Yashashree  Bedmutha <yashashree.bedmutha@gmail.com>
2023-01-31 20:07:43 +0530

    Time: 369 ms (94.14%), Space: 16.8 MB (80.61%) - LeetHub

---
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
@@ -0,0 +1,24 @@
+class Solution:
+    # Binary Search Approach
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        N, M = len(nums1), len(nums2)
+        
+        def ok(k):
+            # the idea is to use binary search to find the length `k`
+            # then we check if there is any nums1[i : i + k] == nums2[i : i + k]
+            s = set(tuple(nums1[i : i + k]) for i in range(N - k + 1))
+            return any(tuple(nums2[i : i + k]) in s for i in range(M - k + 1))
+        
+        # init possible boundary
+        l, r = 0, min(N, M)
+        while l < r:
+            # get the middle one
+            # for even number of elements, take the upper one
+            m = (l + r + 1) // 2
+            if ok(m): 
+                # include m
+                l = m
+            else:
+                # exclude m
+                r = m - 1
+        return l
