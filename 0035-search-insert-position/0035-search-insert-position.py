289bf8b267409f9c99776f522b51e72e7f300190
Yashashree  Bedmutha <yashashree.bedmutha@gmail.com>
2023-02-01 21:57:44 +0530

    Time: 29 ms (90.76%), Space: 14.1 MB (83.41%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        for i in range(len(nums)):
+            if(nums[i] >= target):
+                return i
+        return len(nums)
