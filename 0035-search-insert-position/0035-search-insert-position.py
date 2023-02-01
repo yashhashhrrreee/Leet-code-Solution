bbe42e9853725fddbd061a6e2333fc49417aabb0
Yashashree  Bedmutha <yashashree.bedmutha@gmail.com>
2023-02-01 21:58:06 +0530

    Time: 39 ms (99.48%), Space: 14.7 MB (76.28%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,5 +1,6 @@
-class Solution(object):
-    def searchInsert(self, nums, target):
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        #Traverse all elements through the loop...
         for i in range(len(nums)):
             if(nums[i] >= target):
                 return i
