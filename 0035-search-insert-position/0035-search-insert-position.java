7bc3ae1d546314ff1427c4c9e97859babd06edec
Yashashree  Bedmutha <yashashree.bedmutha@gmail.com>
2023-02-20 09:12:15 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (61.63%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,11 +1,20 @@
-// The insert position is the index which element’s value is greater or equal with target.
-// So we scan linearly the array will find the answer.
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        //Traverse all elements through the loop...
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] >= target)   return i;
+        int start = 0;
+        int end = nums.length - 1;
+        int ans = 0;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target){
+                ans = mid;
+                break;
+            }else if(nums[mid] > target){
+                end = mid - 1;
+            }else{
+                ans = mid + 1;
+                start = mid + 1;
+            }
         }
-    return nums.length;
+        return ans;
     }
 }
