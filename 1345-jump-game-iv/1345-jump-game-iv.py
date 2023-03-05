776a67b3a6befe3be4330b953d4771697c2fdbf0
Yashashree  Bedmutha <yashashree.bedmutha@gmail.com>
2023-03-06 00:21:32 +0530

    Time: 682 ms (95.94%), Space: 26.9 MB (99.19%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minJumps(self, arr: List[int]) -> int:
+        n = len(arr)
+        vis = [False] * n
+        vis[0] = True
+        q = [0]
+        d = defaultdict(list)
+        for i in range(n):
+            d[arr[i]].append(i)
+        ans = -1
+        while q:
+            ans += 1
+            q2 = []
+            for i in q:
+                if i == n-1:
+                    return ans
+                if i > 0 and not vis[i-1]:
+                    q2.append(i-1)
+                    vis[i-1] = True
+                if i < n-1 and not vis[i+1]:
+                    q2.append(i+1)
+                    vis[i+1] = True
+                for j in d[arr[i]]:
+                    if not vis[j]:
+                        q2.append(j)
+                        vis[j] = True
+                d[arr[i]].clear() # optimization
+            q = q2
+        return -1 # not found, unreachable
